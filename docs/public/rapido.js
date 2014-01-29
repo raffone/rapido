71e0c95148cea0af664894f4f735db6f30750764
Raffaele Rasini <raffaele.rasini@gmail.com>
2014-01-29 15:38:33 +0100

    Added tooltip support fro window.resize Removed unnecessary code Updated docs and dist

--- docs/public/rapido.js
+++ docs/public/rapido.js
@@ -631,51 +631,41 @@ $('.offcanvas__menu--toggle').rapido_Offcanvas();
 
     base.$el.data('Rapido.Tooltip', base);
 
+    var target = {};
+    var tooltip = {};
+    var content = base.$el.data('tooltip-content');
+    var position = base.$el.data('tooltip-position');
+    var is = {
+      top: position.indexOf('top') >= 0,
+      right: position.indexOf('right') >= 0,
+      bottom: position.indexOf('bottom') >= 0,
+      left: position.indexOf('left') >= 0,
+      center: position.indexOf('center') >= 0
+    };
+
     base.init = function() {
       base.options = $.extend({},$.Rapido.Tooltip.defaultOptions, options);
 
       addTooltip();
     };
 
+    // Get position of target
+    var getTargetData = function() {
 
-    var addTooltip = function() {
-
-      // Create empty objects
-      var target = {};
-      var tooltip = {};
-
-      // Get content of tooltip
-      var content = base.$el.data('tooltip-content');
-
-      // Get positioning settings of tooltip
-      var position = base.$el.data('tooltip-position');
-
-      // Add tooltip to html
-      $('<span class="tooltip-content" />').html('<span>' + content + '</span>').insertAfter(base.el);
-      //$('<span class="tooltip-content" />').appendTo('body');
-
-
-      // Get position of target
       target.top = base.$el.position().top;
       target.left = base.$el.position().left;
       target.height = base.$el.outerHeight();
       target.width = base.$el.outerWidth();
+    };
 
+    // Get size of tooltip
+    var getTooltipData = function() {
+      tooltip.height = base.$el.next('.tooltip').outerHeight();
+      tooltip.width = base.$el.next('.tooltip').outerWidth();
+    };
 
-      // Get size of tooltip
-      tooltip.height = $('.tooltip-content').outerHeight();
-      tooltip.width = $('.tooltip-content').outerWidth();
-
-      // Check what position is set in the options
-      var is = {
-        top: position.indexOf('top') >= 0,
-        right: position.indexOf('right') >= 0,
-        bottom: position.indexOf('bottom') >= 0,
-        left: position.indexOf('left') >= 0,
-        center: position.indexOf('center') >= 0
-      };
-
-      // Calculate positioning
+    // Calculate positioning
+    var setTooltipPositioning = function() {
       if (is.top) {
         tooltip.top = target.top - tooltip.height - base.options.margin;
       }
@@ -688,12 +678,6 @@ $('.offcanvas__menu--toggle').rapido_Offcanvas();
       if (is.left) {
         tooltip.left = target.left - tooltip.width - base.options.margin;
       }
-      //if (is.top && is.center || is.bottom && is.center) {
-      //tooltip.left = target.left + ( target.width / 2) - ( tooltip.width / 2);
-      //}
-      //if (is.left && is.center || is.right && is.center) {
-      //tooltip.top = target.top + ( target.height / 2) - ( tooltip.height / 2);
-      //}
       if (is.top || is.bottom) {
         tooltip.left = target.left + (target.width / 2) - (tooltip.width / 2);
       }
@@ -704,21 +688,35 @@ $('.offcanvas__menu--toggle').rapido_Offcanvas();
       // Reset width and height, we don't need them anymore
       tooltip.height = null;
       tooltip.width = null;
-
-      // Add css positioning to tooltip
-      base.$el.next('.tooltip-content').css(tooltip);
-
-      base.$el.on('mouseenter mouseleave', function() {
-        base.$el.next('.tooltip-content').toggleClass('open');
-      });
-
-      //base.$el.on('mouseleave', function(){
-      //base.$el.next('.tooltip-content').removeClass('open')
-      //});
-
     };
 
+    // Add tooltip to html
+    var addTooltip = function() {
+      $('<span class="tooltip" />').html(content).insertAfter(base.el);
 
+      // Get and set correct positioning
+      getTargetData();
+      getTooltipData();
+      setTooltipPositioning();
+
+      $(window).resize(function() {
+        getTargetData();
+        getTooltipData();
+        setTooltipPositioning();
+      });
+
+      // Add css positioning to tooltip
+      base.$el.next('.tooltip').css(tooltip);
+
+      $(window).resize(function() {
+        base.$el.next('.tooltip').css(tooltip);
+      });
+
+      // Toggle on :hover
+      base.$el.on('mouseenter mouseleave', function() {
+        base.$el.next('.tooltip').toggleClass('open');
+      });
+    };
 
     base.init();
   };
--- js/rapido.tooltip.js
+++ js/rapido.tooltip.js
@@ -12,51 +12,41 @@
 
     base.$el.data('Rapido.Tooltip', base);
 
+    var target = {};
+    var tooltip = {};
+    var content = base.$el.data('tooltip-content');
+    var position = base.$el.data('tooltip-position');
+    var is = {
+      top: position.indexOf('top') >= 0,
+      right: position.indexOf('right') >= 0,
+      bottom: position.indexOf('bottom') >= 0,
+      left: position.indexOf('left') >= 0,
+      center: position.indexOf('center') >= 0
+    };
+
     base.init = function() {
       base.options = $.extend({},$.Rapido.Tooltip.defaultOptions, options);
 
       addTooltip();
     };
 
+    // Get position of target
+    var getTargetData = function() {
 
-    var addTooltip = function() {
-
-      // Create empty objects
-      var target = {};
-      var tooltip = {};
-
-      // Get content of tooltip
-      var content = base.$el.data('tooltip-content');
-
-      // Get positioning settings of tooltip
-      var position = base.$el.data('tooltip-position');
-
-      // Add tooltip to html
-      $('<span class="tooltip" />').html('<span>' + content + '</span>').insertAfter(base.el);
-      //$('<span class="tooltip" />').appendTo('body');
-
-
-      // Get position of target
       target.top = base.$el.position().top;
       target.left = base.$el.position().left;
       target.height = base.$el.outerHeight();
       target.width = base.$el.outerWidth();
+    };
 
+    // Get size of tooltip
+    var getTooltipData = function() {
+      tooltip.height = base.$el.next('.tooltip').outerHeight();
+      tooltip.width = base.$el.next('.tooltip').outerWidth();
+    };
 
-      // Get size of tooltip
-      tooltip.height = $('.tooltip').outerHeight();
-      tooltip.width = $('.tooltip').outerWidth();
-
-      // Check what position is set in the options
-      var is = {
-        top: position.indexOf('top') >= 0,
-        right: position.indexOf('right') >= 0,
-        bottom: position.indexOf('bottom') >= 0,
-        left: position.indexOf('left') >= 0,
-        center: position.indexOf('center') >= 0
-      };
-
-      // Calculate positioning
+    // Calculate positioning
+    var setTooltipPositioning = function() {
       if (is.top) {
         tooltip.top = target.top - tooltip.height - base.options.margin;
       }
@@ -69,12 +59,6 @@
       if (is.left) {
         tooltip.left = target.left - tooltip.width - base.options.margin;
       }
-      //if (is.top && is.center || is.bottom && is.center) {
-      //tooltip.left = target.left + ( target.width / 2) - ( tooltip.width / 2);
-      //}
-      //if (is.left && is.center || is.right && is.center) {
-      //tooltip.top = target.top + ( target.height / 2) - ( tooltip.height / 2);
-      //}
       if (is.top || is.bottom) {
         tooltip.left = target.left + (target.width / 2) - (tooltip.width / 2);
       }
@@ -85,21 +69,35 @@
       // Reset width and height, we don't need them anymore
       tooltip.height = null;
       tooltip.width = null;
+    };
+
+    // Add tooltip to html
+    var addTooltip = function() {
+      $('<span class="tooltip" />').html(content).insertAfter(base.el);
+
+      // Get and set correct positioning
+      getTargetData();
+      getTooltipData();
+      setTooltipPositioning();
+
+      $(window).resize(function() {
+        getTargetData();
+        getTooltipData();
+        setTooltipPositioning();
+      });
 
       // Add css positioning to tooltip
       base.$el.next('.tooltip').css(tooltip);
 
+      $(window).resize(function() {
+        base.$el.next('.tooltip').css(tooltip);
+      });
+
+      // Toggle on :hover
       base.$el.on('mouseenter mouseleave', function() {
         base.$el.next('.tooltip').toggleClass('open');
       });
-
-      //base.$el.on('mouseleave', function(){
-      //base.$el.next('.tooltip').removeClass('open')
-      //});
-
     };
-
-
 
     base.init();
   };
