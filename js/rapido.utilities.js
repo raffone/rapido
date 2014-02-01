6a9c47d7d6cd474331ab9ec84aabf741104c189d
Raffaele Rasini <raffaele.rasini@gmail.com>
2014-02-01 20:57:03 +0100

    Remove white spaces from classes before converting them in getClass Added elemExist function

--- js/rapido.utilities.js
+++ js/rapido.utilities.js
@@ -2,12 +2,20 @@
 
   $.rapido_Utilities = {
 
+    // Get class of element object
     getClass: function(el) {
-      return '.' + $(el).attr('class').split(' ').join('.');
+      el = $(el).attr('class').replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+      return '.' + el.split(' ').join('.');
     },
 
+    // Remove dot from class
     dotlessClass: function(string) {
       return string.slice(1);
+    },
+
+    // Check if an element exist
+    elemExist: function(string) {
+      return $(string).length !== 0;
     }
 
   };
