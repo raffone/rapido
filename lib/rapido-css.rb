b34d3515b054ca431380cc0b10f34fb2624a822e
Raffaele Rasini <raffaele.rasini@gmail.com>
2014-02-28 01:22:37 +0100

    Added sass-globbing and fixed dependecies

--- lib/rapido-css.rb
+++ lib/rapido-css.rb
@@ -1,6 +1,7 @@
 require 'compass'
 require 'susy'
 require 'sass-media_query_combiner'
+require 'sass-globbing'
 
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('rapido-css', :path => extension_path)
