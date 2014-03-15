2bd7e4480c0b45e1b9a36969ff2094f9bf4d9025
Raffaele Rasini <raffaele.rasini@gmail.com>
2014-03-15 23:12:24 +0100

    Added breakpoint and compass-placeholders

--- lib/rapido-css.rb
+++ lib/rapido-css.rb
@@ -1,7 +1,9 @@
 require 'compass'
+require 'breakpoint'
 require 'susy'
 require 'sass-media_query_combiner'
 require 'sass-globbing'
+require 'compass-placeholders'
 
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('rapido-css', :path => extension_path)
